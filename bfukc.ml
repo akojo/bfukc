01a7bd9c8c68b2097607b1af3eceaca46a4f3cfa
Atte Kojo <atte.kojo@reaktor.com>
2016-03-28 20:23:58 +0300

    Add option to interpret code directly

--- bfukc.ml
+++ bfukc.ml
@@ -1,15 +1,22 @@
 open Core.Std
 
-let run file () =
+let tape_size = 131072
+
+let run interpret file () =
+  let runner = if interpret then Interpreter.run else Generator.run in
   let () = In_channel.with_file file ~f:Compiler.read
     |> Compiler.optimize
-    |> Generator.run 131072 in
+    |> runner tape_size in
   Out_channel.flush stdout
 
 let command =
   Command.basic
     ~summary:"Optimizing BrainFuck compiler"
-    Command.Spec.(empty +> anon ("filename" %: string))
+    Command.Spec.(
+      empty
+      +> flag "-b" no_arg ~doc:" compile and run immediately using a bytecode interpreter"
+      +> anon ("filename" %: string)
+      )
     run
 
 let () =
