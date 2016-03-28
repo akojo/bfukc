9c2121d56f297b15a5ba99eebb61177bc48fb6c5
Atte Kojo <atte.kojo@reaktor.com>
2016-03-28 20:19:27 +0300

    Add simple command-line interface

--- bfukc.ml
+++ bfukc.ml
@@ -1,7 +1,16 @@
 open Core.Std
 
-let () =
-  let () = Compiler.read stdin
+let run file () =
+  let () = In_channel.with_file file ~f:Compiler.read
     |> Compiler.optimize
     |> Generator.run 131072 in
   Out_channel.flush stdout
+
+let command =
+  Command.basic
+    ~summary:"Optimizing BrainFuck compiler"
+    Command.Spec.(empty +> anon ("filename" %: string))
+    run
+
+let () =
+  Command.run ~version:"1.0" ~build_info:"Atte Kojo <atte.kojo@gmail.com>" command
